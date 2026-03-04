53ac085dcc19a17e70388b09d579c570cb77ab42
Abdullah Mohammed Salman <43343208+wacns@users.noreply.github.com>
2026-03-04 03:49:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿require('dotenv').config();
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot is running");
+}).listen(PORT, "0.0.0.0", () => {
+  console.log(`Health server running on port ${PORT}`);
+});
+
+﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿require('dotenv').config();
 const { 
     Client, GatewayIntentBits, Collection, EmbedBuilder, 
     ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, AttachmentBuilder,
